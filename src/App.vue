5ef9ca0220fb8d683f4842822b2627eef3a59efb
lynickyang <likesheep@outlook.com>
2024-02-18 19:05:00 +0800

    完成星星點擊

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup>
 import { items } from "./movies.json";
 import "bootstrap-icons/font/bootstrap-icons.scss";
+import {ref} from 'vue';
+
+const movies = ref(items);
+
+function getStartNumber(index,i){
+  console.log("item:",index)
+  console.log("i:",i)
+  movies.value[index].rating=i;
+
+}
 /*
  This is an Icon that you can use to represent the stars if you like
  otherwise you could just use a simple ⭐️ emoji, or * character.
@@ -12,7 +22,7 @@ import "bootstrap-icons/font/bootstrap-icons.scss";
   <!-- This is where your template goes -->
   <div class="container">
     <div class="row row-cols-3 my-4">
-      <div v-for="item in items" :key="item.id" class="col">
+      <div v-for="( item,index ) in movies" :key="item.id" class="col">
         <div class="card">
           <img
             :src="item.image"
@@ -35,8 +45,15 @@ import "bootstrap-icons/font/bootstrap-icons.scss";
           <div class="card-footer border-0 ">
             <div class="row d-inline">
                 <p class="card-text d-inline">Rationg:{{item.rating}}/5</p>
-                <div v-for="i in 5" :key="{i}" class="row d-inline">
-                  <div class="d-inline" :class="i<= item.rating? 'text-warning': ''"><i class="bi bi-star-fill "></i></div>
+                <div class="d-inline">
+                  <button v-for="i in 5" :key="{i}" >
+                    <div class="d-inline" 
+                    :class="i<= item.rating? 'text-warning': ''"
+                    @click="getStartNumber(index,i)"
+                    >
+                    <i class="bi bi-star-fill "></i>
+                    </div>
+                  </button>
                 </div>
               </div> 
           </div>
